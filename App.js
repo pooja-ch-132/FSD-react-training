92845be1fd95eea1f67a793ff3e4dbb13b34ced9
pooja-ch-132 <158035573+pooja-ch-132@users.noreply.github.com>
2025-02-12 21:02:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Classroom from "./pages/Classroom";
+import Assessments from "./pages/Assessments";
+import News from "./pages/News";
+import Calendar from "./pages/Calendar";
+import Store from "./pages/Store";
+import Blog from "./pages/Blog";
+import PageNotFound from "./pages/PageNotFound";
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/classroom" element={<Classroom />} />
+        <Route path="/assessments" element={<Assessments />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </div>
+  );
+}
+export default App;
---
+++ pages/PageNotFound.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      PageNotFound
+      <h1>
+        404!
+        <br />
+        PageNotFound
+      </h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
